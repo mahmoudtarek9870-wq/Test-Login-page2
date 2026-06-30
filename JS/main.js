1ea43c374f840c9430ff1a56eb78ad7974303045
kali <kali@kali>
2026-06-30 19:08:00 +0300

    First commit

---
+++ JS/main.js
@@ -0,0 +1,26 @@
+/*
+    document.createElement("")
+    console.table([""]) 
+        ----------------------------
+
+        Data Types And Typeof Operator :
+
+            - String
+            - Number
+            - Array
+            - Kay and Value
+            - boolean
+            - undefined
+            - null
+        
+        ----------------------------
+
+        Variables Introduction
+
+            - Syntax (Kayword | Variable Name | Assignment Operator | Variable Value)
+ */
+
+
+var name = "Mahmoud"
+
+console.log(name)
